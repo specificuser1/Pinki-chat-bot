3b1a434017296935964b2b7ec3f613ec9523ec4c
specificuser1 <motorolag14rx@gmail.com>
2026-02-22 01:22:37 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,23 @@ from discord import app_commands
 from groq import Groq
 import json, os, time, requests
 from dotenv import load_dotenv
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot Alive"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
 
 load_dotenv()
 
